cdcaf29b97cd374ae00d2b4ba8723e6ac76a65cc
Mathilde <nihaomouw@mac.com>
2019-09-16 11:44:14 -0700

    Start using ofTildeblobPrimitive

--- src/ofApp.h
+++ src/ofApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofTildeblobPrimitive.hpp"
 
 class ofApp : public ofBaseApp{
 
@@ -21,7 +22,7 @@ class ofApp : public ofBaseApp{
     void dragEvent(ofDragInfo dragInfo);
     void gotMessage(ofMessage msg);
 
-    ofCylinderPrimitive cylinder;
+    ofTildeblobPrimitive cylinder;
     ofMaterial material;
 
     // place to store the sides of the box //
